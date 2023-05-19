bea91de278c2c2ffb0942eaa3dbd2a698edf543a
Nkechinyere Urum-Eke <nk@nkechinyeresmbp.myfiosgateway.com>
2023-05-18 20:02:28 -0400

    for your review

---
+++ app2.js
@@ -0,0 +1,53 @@
+class Casino {
+  constructor(name, fakeCoin) {
+    this.name = name;
+    this.timesPlayed = 0;
+    this.winnings = 0;
+    this.fakeCoin = fakeCoin;
+  }
+
+  playGame(betAmount) {
+    this.timesPlayed += 1;
+    if (Math.random() > 0.5 && this.fakeCoin == false) {
+      console.log("You won " + betAmount *
+        (this.timesPlayed) + " dollars from "
+        + this.name + "!");
+      this.winnings += this.timesPlayed * betAmount;
+      console.log("Current winnings: " + this.winnings);
+    } else {
+      console.log(this.name + " wins!");
+      this.timesPlayed = 0;
+      this.winnings = 0;
+    }
+  }
+
+  rollDice(sides) {
+    console.log(Math.floor(Math.random() * sides) + 1);
+  }
+}
+
+// TESTS
+const myCasino = new Casino("HackerU Casino", false);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.playGame(5);
+myCasino.rollDice(6);
+myCasino.rollDice(6);
+myCasino.rollDice(6);
+myCasino.rollDice(6);
+myCasino.rollDice(6);
+myCasino.rollDice(6);
+myCasino.rollDice(6);
+myCasino.rollDice(1000);
